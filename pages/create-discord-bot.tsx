171e553e01ebf8ffc0f74f88247660c23ba86a94
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-12-11 14:08:57 -0600

    +

--- pages/create-discord-bot.tsx
+++ pages/create-discord-bot.tsx
@@ -13,7 +13,7 @@ const CreateDiscordBot: NextPage = () => {
         <h1 className={g['text-large']}>CREATE A</h1>
         <SpecialTextP gradient='#5865f2' text='DISCORD' textSize='large' bg='/backgrounds/rain_blurred.png' textTransparent gap direction='ltr' />
         <h1 className={g['text-large']}>BOT</h1>
-        <h1 className={g['text-large']}>EPISODE 1</h1>
+        <h1 className={g['text-large']}>EPISODE 3</h1>
       </TextContainer>
       <div className={g['side-container']} style={{ marginLeft: 'auto', marginRight: '60px' }}>
         <Image src={discord} width={800} alt='' />
---
+++ scripts/ss.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const { join } = require('path');
+
+const pageName = process.argv[2];
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ['--start-maximized']
+  });
+
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:3000/${pageName}`);
+  await page.setViewport({ width: 1920, height: 1080 });
+  await new Promise(r => setTimeout(r, 3000));
+  await page.screenshot({
+    type: 'png',
+    path: join(__dirname, '..', 'outputs', `${pageName}.png`),
+    fullPage: true
+  });
+
+  // browser.close();
+  console.log(`screenshoted http://localhost:3000/${pageName}`);
+})();
